3cb6e2b1be256cb840c40c55024e5b3f2144a390
OscarG-08 <oscarg-ricardos@hotmail.com>
2023-12-27 21:18:21 -0500

    Se agrego funciones para transformar a entero un dato y a flotante otra funcion

--- saludo.js
+++ saludo.js
@@ -1,6 +1,8 @@
 saludar = function(){
     let nombre = recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarInt("txtEstatura");
 }
 
 
@@ -10,4 +12,14 @@ recuperarTexto = function(idComponente){
     componente = document.getElementById(idComponente);
     valorIngresado = componente.value;
     return valorIngresado;
+};
+recuperarInt = function(idComponente){
+    let valorIngresado = recuperarTexto(idComponente);;
+    let valorEntero = parseInt(valorIngresado); 
+    return valorEntero;
+};
+recuperarFloat = function(idComponente){
+    let valorIngresado = recuperarTexto(idComponente);;
+    let valorFlotante = parseFloat(valorIngresado); 
+    return valorFlotante;
 };
